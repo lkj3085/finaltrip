9a39601e53bd1ad0af880e8d210b08c245039b1c
lkj3085 <lkj3085@gmail.com>
2022-02-28 09:39:52 +0900

    finaltrip

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+const profile_pic = require('../assets/images/profile_pic.jpg');
+const profile_pic2 = require('../assets/images/profile_pic2.jpg');
+
+export default {
+  profile_pic,
+  profile_pic2,
+};
